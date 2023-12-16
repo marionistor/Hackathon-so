38621b69c4764b7ac2d738715c1a891a1fbdad84
iragut <ianosiudenco@gmail.com>
2023-12-16 12:26:47 +0200

    Final again

--- content/assignments/lambda-function-loader/src/ipc.c
+++ content/assignments/lambda-function-loader/src/ipc.c
@@ -8,25 +8,32 @@
 #include <unistd.h>
 
 #include "ipc.h"
-#include "../utils/utils.h"
 
 int create_socket(void)
 {
 	int sockfd;
 	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-	DIE(sockfd == -1, "socket");
+	if (sockfd == -1) {
+		fprintf(stderr, "failed socket\n");
+		return -1;
+	}
 	return sockfd;
 }
 
 int connect_socket(int fd)
 {
+	// Create a socket address and connect to the server
 	struct sockaddr_un addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
 	snprintf(addr.sun_path, sizeof(SOCKET_NAME), "%s", SOCKET_NAME);
 	int rc;
 	rc = connect(fd, (struct sockaddr *) &addr, sizeof(addr));
-	DIE(rc < 0, "connect");
+	if (rc == -1) {
+		fprintf(stderr, "failed connect\n");
+		return -1;
+	}
+	
 	return rc;
 }
 
@@ -38,10 +45,12 @@ ssize_t send_socket(int fd, const char *buf, size_t len)
 	// Write to the socket until the end of the buffer
 	while (send_id < len) {
 		counter = write(fd, buf + send_id, len - send_id);
-		DIE(counter == -1, "send_socket");
-		if (counter == 0)
+		if (counter == -1) {
+			fprintf(stderr, "failed send\n");
+			return -1;
+		} else if (counter == 0) {
 			break;
-
+		}	
 		send_id += counter;
 	}
 
@@ -56,9 +65,12 @@ ssize_t recv_socket(int fd, char *buf, size_t len)
 	// Read from the socket until the end of the buffer
 	while (recv_id < len) {
 		counter = read(fd, buf + recv_id, len - recv_id);
-		DIE(counter == -1, "recv_socket");
-		if (counter == 0)
+		if (counter == -1) {
+			fprintf(stderr, "failed recv\n");
+			return -1;
+		} else if (counter == 0) {
 			break;
+		}
 		recv_id += counter;
 	}
 
@@ -69,5 +81,7 @@ void close_socket(int fd)
 {
 	int rc;
 	rc = close(fd);
-	DIE(rc < 0, "clozz`se");
+	if (rc == -1) {
+		fprintf(stderr, "failed close\n");
+	}
 }
