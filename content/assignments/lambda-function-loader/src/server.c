3bac257ef11d0f7d05a739092e920877c1b8bb47
iragut <ianosiudenco@gmail.com>
2023-12-16 14:17:33 +0200

    Load?

--- content/assignments/lambda-function-loader/src/server.c
+++ content/assignments/lambda-function-loader/src/server.c
@@ -21,13 +21,33 @@
 static int lib_prehooks(struct lib *lib)
 {
 	/* TODO: Implement lib_prehooks(). */
+	lib->libname = (char *) calloc(sizeof(char) * BUFSIZE);
+	if (lib->libname == NULL)
+		return -1;
+	lib->funcname = (char *) calloc(sizeof(char) * BUFSIZE);
+	if (lib->funcname == NULL)
+		return -1;
+	lib->filename = (char *) calloc(sizeof(char) * BUFSIZE);
+	if (lib->filename == NULL)
+		return -1;
+	lib->outputfile = (char *) calloc(sizeof(char) * BUFSIZE);
+	if (lib->outputfile == NULL)
+		return -1;
+
+	strcpy(lib->outputfile, OUTPUT_TEMPLATE);
+	int err = mkstemp(lib->outputfile);
+	if (err == -1)
+		return -1;
 
 	return 0;
 }
 
 static int lib_load(struct lib *lib)
 {
-	/* TODO: Implement lib_load(). */
+	lib->handle = dlopen(lib->filename, RTLD_LAZY);
+	if (lib->handle == NULL)
+		return -1;
+	
 	return 0;
 }
 
@@ -91,34 +111,49 @@ int main(void)
 	struct sockaddr_un addr, raddr;
 	socklen_t raddrlen;
 	int listenfd, connectfd;
-	char buf[BUFSIZE];
+	char buf[BUFSIZE], name[BUFSIZE], func[BUFSIZE], params[BUFSIZE];
 
 	remove(SOCKET_NAME);
 
 	listenfd = create_socket();
-	// todo err
+	if (listenfd == -1) {
+		perror("unix socket");
+		return -1;
+	}
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
 	snprintf(addr.sun_path, sizeof(SOCKET_NAME), "%s", SOCKET_NAME);
 	ret	= bind(listenfd, (struct sockaddr *) &addr, sizeof(addr));
-	// todo err
+	if (ret == -1) {
+		perror("bind unix socket");
+		return -1;
+	}
 
 	ret = listen(listenfd, MAX_CLIENTS);
+	if (ret == -1) {
+		perror("listen unix socket");
+		return -1;
+	}
 
 	while (1) {
 		/* TODO - get message from client */
 
-		//printf("before\n");
 		connectfd = accept(listenfd, (struct sockaddr *) &raddr, &raddrlen);
-		// todo err
-		//printf("after\n");
+		if (connectfd == -1) {
+			perror("accept unix socket");
+			return -1;
+		}
 
 		memset(buf, 0, BUFSIZE);
 		ssize_t recv_id = recv_socket(connectfd, buf, BUFSIZE);
 
-		send_socket(connectfd, buf, BUFSIZE);
+		lib_prehooks(&lib);
 
-		printf("%s\n", buf);
+		parse_command(buf, lib.libname, lib.funcname, lib.filename);
+
+		// send_socket(connectfd, buf, BUFSIZE);
+
+		// printf("%s\n", buf);
 
 		/* TODO - parse message with parse_command and populate lib */
 		/* TODO - handle request from client */
